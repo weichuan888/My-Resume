dbe6217b3b1f1f54759329b64143094808db3ca5
Weichuan <weichuan.tran@gmail.com>
2018-03-21 10:05:41 +0100

    update

--- JS/ReactJS/my-app/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import './index.css';
-import Welcome from './Welcome';
-
-
-ReactDom.render(<Welcome/>, document.getElementById('root'));
